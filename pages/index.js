3eb1fbf4dfb2c70fa58e7b8caa5a6b8f02183ae9
Juarez Junior <112733205+Jjunior112@users.noreply.github.com>
2025-08-13 18:58:46 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home()
 {
-    return <h1>Adrielly, eu amo você. Se você me ama, respira! </h1>
+    return <h1>Adrielly, eu amo você. Se você me ama, respira! &#10084; </h1>
 }
 
 export default Home;
