b482d63cb197e8c666ee3354b6d76d7bac7d2064
Juarez Junior <112733205+Jjunior112@users.noreply.github.com>
2025-08-19 17:03:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de estudos do curso.dev para aprender um projeto do início ao fim.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home()
-{
-    return <h1>Adrielly, eu amo você. Se você me ama, respira! &#10084; </h1>
+function Home() {
+  return <h1>Adrielly, eu amo você. Se você me ama, respira! &#10084; </h1>;
+}
+
+function teste1() {
+  console.log("indentação errada!");
 }
 
 export default Home;
