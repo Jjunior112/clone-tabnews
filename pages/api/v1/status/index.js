0c1ce4e681a25e2808c2b96100a224383f735979
Juarez Junior <112733205+Jjunior112@users.noreply.github.com>
2025-09-01 21:37:46 +0000

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,33 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 +1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
 
-  response.status(200).json({ chave: "Hello world!" });
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConn = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseUsedConnResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseUsedConn = databaseUsedConnResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_conn: parseInt(databaseMaxConn),
+        used_conn: databaseUsedConn,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,16 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  console.log(responseBody);
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_conn).toEqual(100);
+  expect(responseBody.dependencies.database.used_conn).toEqual(1);
 });
