9f2fa7470fed93c38285c32df3c8964abd7095da
Juarez Junior <112733205+Jjunior112@users.noreply.github.com>
2025-09-08 19:04:12 +0000

    adds àpi/v1/migrations` endpoint

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,13 +1,21 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST to /api/v1/migratons should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
 
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  const responseBody = await response.json();
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
 
-  console.log(responseBody);
-
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(response2.status).toBe(200);
 });
